1e63f7ef8206e5c4d7b5c8628753e131e41755e7
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-07-13 10:28:53 +0700

    z

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập hệ thống</title>
+    <link rel="stylesheet" href="res/css/reset.css">
+    <link rel="stylesheet" href="res/css/login.css">
+</head>
+<body>
+    <div class="login-main">
+        <div class="left-container">
+
+        </div>
+        <div class="right-container">
+            <h1>Đăng nhập</h1>
+            <form method="POST" action="" >
+                
+                <input type="text" placeholder="Tên đăng nhập" name="username_tx">
+                <input type="password" placeholder="Mật khẩu" name="password_tx">
+                <input type="submit" value="Đăng nhập">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
